31611632a8ded99c8a71df8d6dcb8404ea95de43
Nashville CCR <admin@nashvilleccr.com>
2025-09-21 13:17:16 -0500

    Fix bug related to SCF local json in dev mode

--- includes/php/register-fields.php
+++ includes/php/register-fields.php
@@ -8,8 +8,14 @@ class RegisterFields {
 
         if (defined('DEV')) {
             add_filter('acf/json/save_paths', [self::class, 'acf__json__save_paths'], 10, 2);
-            add_filter('acf/prepare_field_group_for_import', [self::class, 'acf__prepare_field_group_for_import'], 20, 1);
-            add_filter('acf/prepare_field_group_for_export', [self::class, 'acf__prepare_field_group_for_export'], 20, 1);
+            add_filter('acf/prepare_field_group_for_import', [self::class, 'prepare_json_for_import'], 20, 1);
+            add_filter('acf/prepare_field_group_for_export', [self::class, 'prepare_json_for_export'], 20, 1);
+            add_filter('acf/prepare_post_type_for_import', [self::class, 'prepare_json_for_import'], 20, 1);
+            add_filter('acf/prepare_post_type_for_export', [self::class, 'prepare_json_for_export'], 20, 1);
+            add_filter('acf/prepare_taxonomy_for_import', [self::class, 'prepare_json_for_import'], 20, 1);
+            add_filter('acf/prepare_taxonomy_for_export', [self::class, 'prepare_json_for_export'], 20, 1);
+            add_filter('acf/prepare_ui_options_page_for_import', [self::class, 'prepare_json_for_import'], 20, 1);
+            add_filter('acf/prepare_ui_options_page_for_export', [self::class, 'prepare_json_for_export'], 20, 1);
         }
     }
 
@@ -23,12 +29,12 @@ class RegisterFields {
         return $paths;
     }
 
-    static function acf__prepare_field_group_for_import($group) {
+    static function prepare_json_for_import($group) {
         $group['private'] = false;
         return $group;
     }
 
-    static function acf__prepare_field_group_for_export($group) {
+    static function prepare_json_for_export($group) {
         $group['private'] = true;
         return $group;
     }
